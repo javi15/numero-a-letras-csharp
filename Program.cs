adda084801263e821f0ba2c4e989bc001de0f1ab
javi15 <javier.fz@outlook.com>
2018-06-20 18:46:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace NumeroATexto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Digite el numero");
+            string num = Console.ReadLine();
+            
+            Numeros NumLetra = new Numeros();
+            //String numero = "15963.70";
+            Console.WriteLine(NumLetra.Convertir(num, true));
+            Console.ReadLine();
+        }
+    }
+}
